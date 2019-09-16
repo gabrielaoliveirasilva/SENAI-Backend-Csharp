544e6c0946fd4484a21e7b4781a8e8bc46a9c354
FIC <FIC@info.local>
2019-09-16 14:20:31 -0300

    novas atualizaçẽ

---
+++ Aula1Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula1Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+        }
+    }
+}
---
+++ Aula1Exercicios/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula1Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+        int valor;    
+        int triplo;
+
+        Console.WriteLine("Digite um numero");
+        valor = int.Parse(Console.ReadLine() );
+
+        triplo = valor*3;
+        Console.WriteLine("valor multiplicado por 3 =   " + triplo);
+
+
+        }
+    }
+}
