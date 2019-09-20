a95b93d8e54b06dc2e467a966282809fa491e03b
FIC <FIC@info.local>
2019-09-20 14:37:12 -0300

    pasta Aula3

---
+++ Aula3ex7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aula3ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /*
+                Elabore um algoritmo que leia 100 números inteiros, maiores que zero e imprima os
+                seguintes resultados:
+                    a) A soma de todos os números impares;
+                    b) O produto de todos os números múltiplos de 3.
+            */
+
+            int numero;
+            int contador    = 1;                 
+            int somaImpares = 0;
+            int produtoM    = 1;
+            
+            while(contador <= 30){
+
+                Console.Write("Digite um numero: ");
+                numero = int.Parse( Console.ReadLine() );
+
+                if(numero > 0){
+
+                    if(numero % 2 != 0){
+                        somaImpares = somaImpares + numero; 
+                    }
+
+                    if(numero % 3 == 0){
+                        produtoM = produtoM * numero;
+                    }
+
+                    contador++;
+                }                
+            }
+
+            Console.WriteLine($"Soma dos ímpares : {somaImpares}");        
+            Console.WriteLine($"Produto dos múltiplos de 3 : {produtoM}");
+        }
+    }
+}
