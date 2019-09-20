e1bd78fc648c5c5edcb0c7f6026d2fe7551c487b
FIC <FIC@info.local>
2019-09-20 16:46:40 -0300

    pasta Aula4

--- Aula4_2/Program.cs
+++ Aula4_2/Program.cs
@@ -6,11 +6,34 @@ namespace Aula4_2
     {
         static void Main(string[] args)
         {
-            int[] vetor = new int[10];
+            int[] vetor1 = new int[10];
+            int[] vetor2 = new int[10];
+            int[] vetor3 = new int[10];
 
-            for(int cont = 0; cont <10; cont++){
-                Console.WriteLine("Digite um valor");
-                vetor[cont] = int.Parse(Console.ReadLine() );
+            for (int cont = 0; cont < 9; cont++)
+            {
+                Console.Write("1Digite um valor");
+                vetor1[cont] = int.Parse(Console.ReadLine());
+            }
+
+
+            for (int cont = 0; cont < 9; cont++)
+            {
+                Console.Write("2Digite um valor");
+                vetor2[cont] = int.Parse(Console.ReadLine());
+            }
+
+            for (int cont = 0; cont < 9; cont++)
+            {
+                vetor3[cont] = vetor1[cont];
+                Console.WriteLine(vetor3[cont]);
+
+            }
+
+            for (int cont = 0; cont < 9; cont++){
+                vetor3[cont] = vetor2[cont-10];
+
+                Console.WriteLine(vetor3[cont]);
             }
 
         }
