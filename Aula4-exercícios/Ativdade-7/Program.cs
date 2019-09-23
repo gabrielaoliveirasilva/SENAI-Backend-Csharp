7c9cc7c7b56d712b252e32c2123724e3fb7471b8
FIC <FIC@info.local>
2019-09-23 13:24:44 -0300

    aula4-exercicios

---
+++ Aula4-exercícios/Ativdade-7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ativdade_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maior  = 0;
+            double indice = 0;
+
+            double[] vetor = new double[10];
+
+            for(int i = 0; i <= 9; i++){
+                Console.Write("Digite um numero: ");
+                vetor[i] = double.Parse(Console.ReadLine());
+
+                if(vetor[i] > maior){
+                    maior  = vetor[i];
+                    indice = i;
+                }
+
+            }
+
+            Console.WriteLine($"Maior: {maior} , Indice: {indice}");
+        }
+    }
+}
