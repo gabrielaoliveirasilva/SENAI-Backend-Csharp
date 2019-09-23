da6a072491b955d30662e89f96a93504f582743c
FIC <FIC@info.local>
2019-09-23 11:14:25 -0300

    Aula4_5

---
+++ Aula4_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] vetor = new int[10];
+
+            for(int i= 1; i <= 9; i++){
+                vetor[i] = i * 2;
+            }
+
+            for(int i= 1; i <= 9; i++){
+                Console.WriteLine(vetor[i]);
+        }
+    }
+}
+}
