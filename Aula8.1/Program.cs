615c8d82aebf333110e855f43665007e06cf8314
FIC <FIC@info.local>
2019-09-26 10:09:43 -0300

    Aula8

---
+++ Aula8.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            
+            try{
+
+                // Escrevemos um bloco de código
+                Console.WriteLine("Digite um número");
+                numero = int.Parse(Console.ReadLine( ));
+
+            }catch(Exception ex){
+
+                // Mostramos a execeção da regra
+                // Erro gerado da aplicação
+                Console.WriteLine("Ops :( Deu ruim." + ex);
+
+            }
+            
+
+        }
+    }
+}
